b8d302a05c3edba715ddfe9fed2ebad475bddbf1
Adam Payne <adam@paynelessdev.tech>
2021-09-29 17:24:20 +1000

    working on ui

--- src/lib/ui.rb
+++ src/lib/ui.rb
@@ -1,3 +1,18 @@
-require 'tty-box'
+prompt = TTY::Prompt.new
+active = true
 
-print TTY::Box.frame "Hello world!"
+def header()
+  title = TTY::Font.new(:standard)
+  box = TTY::Box.frame(width: TTY::Screen.width, height: 8) do
+    title.write("THE   STOA", letter_spacing: 5)
+  end
+  print box
+end
+
+def body()
+  box = TTY::Box.frame(width: TTY::Screen.width, height: 30) do
+   "Niggrst"
+  end
+  print box
+end
+
--- src/stoa.rb
+++ src/stoa.rb
@@ -1,45 +1,15 @@
+# Gems
+require 'pastel'
 require 'tty-box'
-#require_relative /lib/ui.rb
-#require_relative /lib/commands.rb
+require 'tty-font'
+require 'tty-prompt'
+require 'tty-screen'
 
-# def main
+#Library
+require_relative 'lib/ui.rb'
+require_relative 'lib/reader.rb'
+require_relative 'lib/commands.rb'
 
-#   if ARGV.include?("v")
-#     puts "visual mode"
-#   end
+header()
+body()
 
-#   for arg in ARGV
-#   end
-
-#   opt = Hash[*ARGV]
-
-#   print opt
-
-# end
-
-def reader(work, book, line)
-
-  case work
-    when 1 || "med" || "meditations"
-      work = "meditations"
-  end
-
-  quote = File.readlines("text/#{work}/#{book}")[line]
-
-  #print TTY::Box.frame "#{quote}"
-  box = TTY::Box.frame(width: 30, height: 10, title: {top_left: "Meditations", bottom_right: "Book 01"}) do
-    "#{quote}"
-  end
-
-  print box
-end
-
-
-puts "work?"
-work = gets.chomp.to_i
-puts "book?"
-book = gets.chomp.to_i
-puts "line?"
-line = gets.chomp.to_i
-
-reader(work, book, line)
